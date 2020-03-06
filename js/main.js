467631c7002b7969f7d922b2e368a3f623d250dd
Rio Harapan Ps <harapan@arc.itb.ac.id>
2020-03-06 11:51:59 +0700

    Development Page

--- js/main.js
+++ js/main.js
@@ -1,53 +1,69 @@
-$(document).ready(function(){
-
-
-	$("#portfolio-contant-active").mixItUp();
-
-
-	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
-	    singleItem:true,
-	    autoPlay: 3000,
-	});
-
-
-
-
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-
-	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
-
-
-	// Counter
-
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-
+$(window).load(function(){
+     $('.preloader').fadeOut('slow');
 });
 
 
+/* =Main INIT Function
+-------------------------------------------------------------- */
+function initializeSite() {
+
+	"use strict";
+
+	//OUTLINE DIMENSION AND CENTER
+	(function() {
+	    function centerInit(){
+
+			var sphereContent = $('.sphere'),
+				sphereHeight = sphereContent.height(),
+				parentHeight = $(window).height(),
+				topMargin = (parentHeight - sphereHeight) / 2;
+
+			sphereContent.css({
+				"margin-top" : topMargin+"px"
+			});
+
+			var heroContent = $('.hero'),
+				heroHeight = heroContent.height(),
+				heroTopMargin = (parentHeight - heroHeight) / 2;
+
+			heroContent.css({
+				"margin-top" : heroTopMargin+"px"
+			});
+
+	    }
+
+	    $(document).ready(centerInit);
+		$(window).resize(centerInit);
+	})();
+
+	// Init effect 
+	$('#scene').parallax();
+
+};
+/* END ------------------------------------------------------- */
+
+/* =Document Ready Trigger
+-------------------------------------------------------------- */
+$(window).load(function(){
+
+	initializeSite();
+	(function() {
+		setTimeout(function(){window.scrollTo(0,0);},0);
+	})();
+
+});
+/* END ------------------------------------------------------- */
 
 
+$('#countdown').countdown({
+	date: "April 01, 2020 00:00:00",
+	render: function(data) {
+	  var el = $(this.el);
+	  el.empty()
+	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
+	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
+	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
+	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
+	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+	}
+});
