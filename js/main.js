535b4101ac5893d18957338b6f445cdd22eb244f
Rio Harapan Pangihutan <rio_h_pangihutan@linkaja.id>
2020-07-09 21:10:30 +0700

    august

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "April 01, 2020 00:00:00",
+	date: "August 01, 2020 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
